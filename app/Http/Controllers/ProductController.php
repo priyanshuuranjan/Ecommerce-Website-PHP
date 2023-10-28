8772de1aab961fc7a9a078d308b620326ea2f515
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-28 23:31:52 +0530

    updating code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Models\Cart;
-use App\Models\Order;
+
 
 class ProductController extends Controller
 {
-function index(){
-    return "Welcome to product page";
-}
+    function index()
+    {
+        $data = Product::all();
+        return view('product');
+    }
 }
