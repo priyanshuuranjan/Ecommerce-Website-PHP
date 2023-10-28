d04a1c3f5a7ae373bfde5b284934178168fb75ab
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-28 18:07:07 +0530

    updating code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,6 +12,7 @@ class UserController extends Controller
     function login(Request $req)
     {
         $user= User::where(['email'=>$req->email])->first();
+        return $user->password;
         if(!$user || !Hash::check($req->password,$user->password))
         {
             return "Username or password is not matched";
