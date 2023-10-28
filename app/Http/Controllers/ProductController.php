be4fb4490134a0fce0ab9b2ba7c0d8e66092a0a3
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-28 18:01:56 +0530

    updating code

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+function index(){
+    return "Welcome to product page"
+}
+}
