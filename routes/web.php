559ca6c73262bc1a7510131b10e8ce56f7105943
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-31 02:24:04 +0530

    Working on order function

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,6 @@ Route::get("detail/{id}",[ProductController::class,'detail']);
 Route::post("add_to_cart",[ProductController::class,'addToCart']);
 Route::get("cartlist",[ProductController::class,'cartList']);
 Route::get("removecart/{id}",[ProductController::class,'removeCart']);
-// Route::get("ordernow",[ProductController::class,'orderNow']);
+Route::get("ordernow",[ProductController::class,'orderNow']);
 // Route::post("orderplace",[ProductController::class,'orderPlace']);
 // Route::get("myorders",[ProductController::class,'myOrders']);
