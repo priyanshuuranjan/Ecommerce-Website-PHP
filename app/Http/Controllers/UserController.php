9eb77d5b8f1602338279bcedd5e05b76b8028ab6
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-31 03:06:23 +0530

    Working on Registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
        $user = new User;
        $user->name=$req->name;
        $user->email=$req->email;
-       $user->password=Hash::make($req->name);
+       $user->password=Hash::make($req->password);
        $user->save();
        return redirect("/login");
     }
