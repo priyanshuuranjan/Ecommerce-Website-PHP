2cdc8dd97d25d5aa1da5b8ef5331cfa38560c35c
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-31 02:54:40 +0530

    Working on Login

--- routes/web.php
+++ routes/web.php
@@ -30,13 +30,13 @@ Route::get('/about', function () {
     return view('about');
 });
 
-
-Route::post("/login",[UserController::class,'login']);
-Route::get("/",[ProductController::class,'index']);
-Route::get("detail/{id}",[ProductController::class,'detail']);
-Route::post("add_to_cart",[ProductController::class,'addToCart']);
-Route::get("cartlist",[ProductController::class,'cartList']);
-Route::get("removecart/{id}",[ProductController::class,'removeCart']);
-Route::get("ordernow",[ProductController::class,'orderNow']);
-Route::post("orderplace",[ProductController::class,'orderPlace']);
-Route::get("myorders",[ProductController::class,'myOrders']);
+Route::post("/register", 'register');
+Route::post("/login", [UserController::class, 'login']);
+Route::get("/", [ProductController::class, 'index']);
+Route::get("detail/{id}", [ProductController::class, 'detail']);
+Route::post("add_to_cart", [ProductController::class, 'addToCart']);
+Route::get("cartlist", [ProductController::class, 'cartList']);
+Route::get("removecart/{id}", [ProductController::class, 'removeCart']);
+Route::get("ordernow", [ProductController::class, 'orderNow']);
+Route::post("orderplace", [ProductController::class, 'orderPlace']);
+Route::get("myorders", [ProductController::class, 'myOrders']);
