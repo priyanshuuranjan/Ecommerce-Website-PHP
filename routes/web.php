10baca61598bcb7d1b3eef25f7e5779d5d3a433b
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-31 03:00:20 +0530

    Working on Login

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,10 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::post("/register", 'register');
+Route::view("/register", 'register');
+
 Route::post("/login", [UserController::class, 'login']);
+Route::post("/register", [UserController::class, 'register']);
 Route::get("/", [ProductController::class, 'index']);
 Route::get("detail/{id}", [ProductController::class, 'detail']);
 Route::post("add_to_cart", [ProductController::class, 'addToCart']);
